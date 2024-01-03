1f38709c63f3efdd8d5b66feda4d8d674334b39b
Matthieu Vion <matthieu.vion@macartevacances.fr>
2024-01-04 00:33:34 +0100

    wezterm updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,9 +5,12 @@ return {
 	initial_rows = 40,
 	scrollback_lines = 9999999,
 	hide_tab_bar_if_only_one_tab = true,
-	color_scheme = 'Molokai',
-  	font = wezterm.font {
-		family = 'JetBrains Mono',
-		harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' },
+	color_scheme = 'Breeze (Gogh)',
+  	font = wezterm.font_with_fallback {
+  		{
+			family = 'JetBrains Mono',
+			harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
+		},
+		'Noto Color Emoji'
     }
 }
